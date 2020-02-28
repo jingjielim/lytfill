085647dfa5d1525d28b625b54a3928fd2883b718
jingjielim <jingjie3000@gmail.com>
2020-02-28 16:04:30 -0500

    Optimise webpage

--- grunt/webpack.js
+++ grunt/webpack.js
@@ -2,8 +2,13 @@
 
 const webpack = require('webpack')
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
   options: {
     entry: {
       application: './index.js',
